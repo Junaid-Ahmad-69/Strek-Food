ee5b388ec77fa0399e599e455e4ebad790d49434
Junaid-Ahmad <junaidsandhu21@gmail.com>
2025-07-15 19:00:24 +0500

    setup the db for food

---
+++ app/meals/[mealSlug]/page.js
@@ -0,0 +1,8 @@
+export default function MealDetailPage({params}){
+    console.log(params.slug)
+    return (
+        <>
+            <h1>MealDetailPage</h1>
+        </>
+    )
+}
---
+++ app/meals/share/page.js
@@ -0,0 +1,7 @@
+export default function ShareMealPage(){
+    return (
+        <>
+            <h1>SharePage</h1>
+        </>
+    )
+}
